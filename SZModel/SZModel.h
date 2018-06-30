abe2c58dbf72e562c2e8e934919fc4ed69a59a14
Song Zhou <zhousong1993@gmail.com>
2018-06-30 16:57:46 +0800

    Initial Commit

---
+++ SZModel/SZModel.h
@@ -0,0 +1,19 @@
+//
+//  SZModel.h
+//  SZModel
+//
+//  Created by songzhou on 2018/6/30.
+//  Copyright © 2018年 songzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SZModel.
+FOUNDATION_EXPORT double SZModelVersionNumber;
+
+//! Project version string for SZModel.
+FOUNDATION_EXPORT const unsigned char SZModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SZModel/PublicHeader.h>
+
+
